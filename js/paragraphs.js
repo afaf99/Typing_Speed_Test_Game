b1a20710f31d4fb1add147b626ee180640f05251
Afaf <afaf60@outlook.sa>
2023-09-04 19:31:36 +0300

    done

---
+++ js/paragraphs.js
@@ -0,0 +1,42 @@
+const paragraphs = [
+
+    "The quick brown fox jumps over the lazy dog. This famous sentence uses every letter in the English alphabet, making it a popular phrase for typing tests.",
+
+    "In the heart of the city, the sun sets behind the skyscrapers. The streets come alive with lights, sounds, and the hustle and bustle of people heading home from work.",
+
+    "The mountain's peak was shrouded in mist. The air was crisp and cold, and the only sound was the crunch of snow under boots. Here, at the top of the world, everything seemed possible.",
+
+    "The library was a sanctuary of silence, filled with the musty smell of old books and the faint rustling of pages being turned. Every book was a portal to a different world, waiting to be explored.",
+
+    "The concert was a riot of noise, color, and energy. The band played their hearts out, the crowd sang along, and for a few hours, everyone forgot their troubles.",
+
+    "The art of cooking is about more than just food. It's about creativity, passion, and sharing something of yourself with others. A well-cooked meal can bring people together like nothing else.",
+
+    "The garden was a riot of colors, from the vibrant red roses to the delicate blue forget-me-nots. The air was filled with the sweet scent of blossoms and the gentle hum of bees.", 
+    
+    "Every morning, the fisherman would take his boat out on the lake. There was something peaceful about watching the sunrise over the water, knowing that the rest of the world was still asleep.",
+    
+    "She sat at her desk, the blank page in front of her a challenge. Writing was a struggle, a fight against the empty page, but it was a fight she was willing to take on.",
+    
+    "The rain fell softly on the city streets, turning them into mirrors that reflected the glowing neon lights. There was a beauty in the city at night that few people ever saw.",
+   
+    "The forest was a kingdom of ancient trees and silent shadows. It was a place of mystery and magic, where every leaf and stone had a story to tell.",
+    
+    "The café was a cozy haven from the busy streets outside. The smell of coffee, the soft murmur of conversation, and the gentle clinking of cups and saucers created a soothing symphony.",
+    
+    "As the sun set, the sky was set ablaze with color. It was as if the day was giving its final, fiery performance before the curtain of night fell.", 
+    
+    "The little town was nestled in the valley, surrounded by rolling hills and fields of wildflowers. It was a place where everyone knew everyone else, and life moved at a slower pace.",
+   
+    "The castle stood tall and proud on the hilltop, its stone walls weathered by centuries of wind and rain. It was a testament to the strength and endurance of the past.",
+  
+    "The jazz club was filled with the smoky air and the soulful sound of a saxophone. It was a place where people came to lose themselves in the music.",
+  
+    "The ocean was a vast expanse of blue, stretching as far as the eye could see. The waves crashed against the shore, a constant reminder of the power of nature.", 
+    
+    "The market was a tapestry of sights, sounds, and smells. Stalls were filled with vibrant fruits, aromatic spices, and the chatter of sellers calling out their wares.",
+   
+    "The river wound its way through the countryside, a shimmering ribbon of blue in a landscape of green. It was a lifeline for the communities that lived along its banks.",
+    
+    "The museum was a treasure trove of art and history. Each exhibit was a window into a different time and place, an invitation to step into the shoes of those who came before us."
+];
